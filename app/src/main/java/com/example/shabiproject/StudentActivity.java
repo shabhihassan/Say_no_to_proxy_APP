57df418c45924b858165894fa4014b943aa9b375
shabhihassan <96010259+shabhihassan@users.noreply.github.com>
2021-12-12 21:25:11 +0500

    first commit

---
+++ app/src/main/java/com/example/shabiproject/StudentActivity.java
@@ -0,0 +1,25 @@
+package com.example.shabiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StudentActivity extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+        btn=findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(StudentActivity.this,StudentPanelActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/shabiproject/ViewTimetableActivity.java
@@ -0,0 +1,17 @@
+package com.example.shabiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ViewTimetableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_timetable);
+
+    }
+}
